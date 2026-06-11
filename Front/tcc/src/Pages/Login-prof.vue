63f6125b7003438b1c89210fbbb544a4490a80ac
bololograus <halesgabriel48@gmail.com>
2026-06-11 17:17:20 -0300

    atulização 3

--- Front/tcc/src/Pages/Login-prof.vue
+++ Front/tcc/src/Pages/Login-prof.vue
@@ -4,39 +4,48 @@ import Registerform from '@/components/Registerform.vue';
 </script>
 
 <template>
-  <div class="login-prof">
-  <section class="left-panel">
-  <div class="overlay"></div>
-  <div class="brand-content">
-    <div class="logo-area">
-      <div class="logo-icon">🛒</div>
-        <h1 class="text-center">Login Professor</h1>
-        <span>Market</span>
-      
-    </div>
-  </div>
-  <p class="description">
-    O mercado escolar que 
-    <span>Recompensa</span> Atitudes!
-  </p>
-  <div class="feature">
-    <div class="icon">🏅</div>
-    <h3>Recompensas</h3>
-    <p>ganhe moeda por boas Atitudes</p>
-  </div>
-  <div class="feature">
-    <div class="icon">🛒</div>
-    <h3>Troque</h3>
-    <p>Troque suas moedas por produtos</p>
-  </div>
-  <div class="feature">
-    <div class="icon">📊</div>
-    <h3>Acompanhe</h3>
-    <p>Veja seu progesso</p>
-  </div>
-  </section>
-  </div>
+    <section class="left-panel">
+      <div class="overlay"></div>
 
+      <div class="brand-content">
+        <div class="logo-area">
+          <div class="logo-icon">🛒</div>
+
+          <h1>
+            Teixeira
+            <span>Market</span>
+          </h1>
+        </div>
+
+        <p class="description">
+          O mercadinho escolar que
+          <span>recompensa</span> atitudes!
+        </p>
+
+        <div class="features">
+
+          <div class="feature">
+            <div class="icon">🏅</div>
+            <h3>Recompensas</h3>
+            <p>Ganhe moedas por boas atitudes</p>
+          </div>
+
+          <div class="feature">
+            <div class="icon">🛒</div>
+            <h3>Troque</h3>
+            <p>Troque suas moedas por produtos</p>
+          </div>
+
+          <div class="feature">
+            <div class="icon">📊</div>
+            <h3>Acompanhe</h3>
+            <p>Veja seu progresso</p>
+          </div>
+
+        </div>
+
+      </div>
+    </section>
 
   <Registerform/>
 </template>
