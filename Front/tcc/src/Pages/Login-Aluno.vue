dd64ee2d8604eb6fafd57151778a9c0db3d2cf2e
mauriciopereiralucas5-gif <mauriciopereiralucas5@gmail.com>
2026-06-11 16:36:04 -0300

    login aluno

--- Front/tcc/src/Pages/Login-Aluno.vue
+++ Front/tcc/src/Pages/Login-Aluno.vue
@@ -3,14 +3,61 @@ import Registerform from '@/components/Registerform.vue';
 </script>
 
 <template>
-  <div class="right-side">
-    <div class="content">
-      <div class="logo">🛒</div>
-      <h1 class="titulo-login-aluno text-center">Bem-vindo!</h1>
-      <Registerform/>
-      <div class="feature"> Ganhe moeda</div>
-    <div class="feature"> Não Falta mais</div>
-    <div class="feature"> Comprar Sua coisa</div>
+    <!--lado esquerdo-->
+    <div class="aluno-page">
+        <div class="overlay"></div>
+        <div class="left-content">
+            <div class="logo">
+          🛒
+        </div>
+        <h1 class="logo-title">
+            Teixeira
+            <span>Market</span>
+        </h1>
+        <p class="subtitle">
+            O mercado do JTA
+            que <span>recompensa</span> por um bom rendimento!
+        </p>
+
+        <div class="features">
+            <div class="feature">
+                <h3>🏅 Recompensas</h3>
+            <p>Ganhe teixeirinhas por um bom desempenho!</p>
+            </div>
+            <div class="feature">
+                <h3>🛒 Troque </h3>
+                <p>Troque teixeirinhas por prêmios!</p>
+            </div>
+            <div class="feature">
+                <h3>📈 Acompanhe</h3>
+                <p>Acompanhe seu rendimento escolar!</p>
+            </div>
+        </div>
+        </div>
+    </div>
+
+<!--lado direito-->
+
+<div class="right-side">
+
+<div class="login-card">
+
+  <p class="welcome">
+    Bem-vindo ao
+  </p>
+
+  <h1 class="titulo-login">
+    Teixeira
+    <span>Market</span>
+  </h1>
+
+  <p class="descricao">
+    Faça login para acessar o site
+  </p>
+
+  <Registerform/>
+
 </div>
+
 </div>
 </template>
